2e0452740c10e766e5cf9beeb05bfba058414255
Rahul Biswas <132062372+rahul-srvpl@users.noreply.github.com>
2023-06-09 18:42:03 +0530

    Update brand_registration.js

--- src/api/routes/v1/brand_registration.js
+++ src/api/routes/v1/brand_registration.js
@@ -6,11 +6,13 @@ router.post(
   brand_registration_controller.create_brand_registration
 );
 
-router.post(
+router.get(
   "/get_registration-data",
   brand_registration_controller.view_register_data
 );
 
-router.post("/get_registration-data/:id", brand_registration_controller.view_single_brand);
+router.get("/get_registration-data/:id", brand_registration_controller.view_single_brand);
+router.get("/seller-info/:sellerId", brand_registration_controller.getSellerInfo)
+
 
 module.exports = router;
